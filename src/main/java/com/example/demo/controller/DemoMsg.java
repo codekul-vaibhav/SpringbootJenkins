cf2bb39bec5b267573cf6135b36fb353650cadfa
vaibhav <codekul.vaibhav@gmail.com>
2021-02-01 15:39:38 +0530

    jenkins method added

--- src/main/java/com/example/demo/controller/DemoMsg.java
+++ src/main/java/com/example/demo/controller/DemoMsg.java
@@ -1,17 +1,21 @@
 package com.example.demo.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoMsg {
 
-
     @GetMapping("/msg")
-    public String  getMsg(){
-
+    public String getMsg() {
         return "Hello Jenkins";
+    }
 
+    @GetMapping("/jenkins")
+    public  String getJenkis(){
+        return "Running jenkins";
     }
 }
+
+
+
